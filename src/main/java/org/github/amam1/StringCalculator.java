d07779a250951c6bed5fc44e6184e1c4624610c2
anna.mleko@gmail.com <konan103>
2016-07-17 23:48:05 +0200

    refactoring and Exception added when negative number occurs

--- src/main/java/org/github/amam1/StringCalculator.java
+++ src/main/java/org/github/amam1/StringCalculator.java
@@ -2,31 +2,56 @@ package org.github.amam1;
 
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public int add(String number) {
+    public int add(String number) throws Exception {
 
-        if(number.equals("")){
+        if (number.equals("")) {
             return 0;
         }
 
-        if(number.split(" ").length > 0){
-            ArrayList<Integer> numbers = new ArrayList<Integer>();
-            String[] parts = number.split(" ");
-            for(String num: parts){
-                numbers.add(Integer.parseInt(num));
-            }
-            Integer sum = 0;
-            for (Integer n: numbers){
-                sum+=n;
-            }
-            return sum;
+        System.out.println();
+
+        Pattern pattern = Pattern.compile("(.*)-\\d(.*)");
+        Matcher matcher = pattern.matcher(number);
+        if(matcher.find()){
+            throw new Exception("negative nor allowed");
         }
 
-        return Integer.parseInt(number);
+        Integer sum = 0;
+        for (Integer n : convertStringToArrayOfIntegers(number)) {
+            sum += n;
+        }
+        return sum;
 
     }
 
+
+    private ArrayList<Integer> convertStringToArrayOfIntegers(String text) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        text = unifyNumberSeparators(text);
+
+        String[] parts = text.split(" ");
+
+        for (String num : parts) {
+            numbers.add(Integer.parseInt(num));
+        }
+
+        return numbers;
+    }
+
+    private String unifyNumberSeparators(String text) {
+        String cleanText = text
+                //all no digits with -
+                .replaceAll("[^\\d]", " ")
+                .replaceAll(" +", " ")
+                .trim();
+
+        System.out.println(cleanText);
+        return cleanText;
+    }
 
 }
--- src/test/java/org/github/amam1/StringCalculatorTest.java
+++ src/test/java/org/github/amam1/StringCalculatorTest.java
@@ -12,24 +12,61 @@ public class StringCalculatorTest {
         // given
 
         // when
-        int result = calculator.add("");
+        int result = 0;
+        try {
+            result = calculator.add("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // then
         assertEquals(result, 0);
     }
 
     @Test
-    public void shouldAdd1ToString() {
+    public void shouldAdd1ToString() throws Exception {
         assertAdd(1, "1");
     }
 
     @Test
-    public void shouldAdd2Numbers() {
+    public void shouldAdd2Numbers() throws Exception {
         assertAdd(2, "1 1");
     }
 
+    @Test
+    public void shouldAdd2Numbers2() throws Exception {
+        assertAdd(51, "45 6");
+    }
 
-    private void assertAdd(int result, String nums) {
-        assertEquals(result, calculator.add(nums));
+
+    @Test
+    public void shouldAdd2NumbersSeparatedByComa() throws Exception {
+        assertAdd(3, "1,2");
+    }
+   @Test
+    public void shouldAddNumbersSeparatedByComaSpaceNewLine() throws Exception {
+        assertAdd(16, "1, 2\n9,4");
+    }
+
+    @Test
+    public void shouldAddNumbersSeparatedByOtherDemiliters() throws Exception {
+        assertAdd(3, "//;\n1;2");
+    }
+
+    @Test
+    public void NegativeNumberShouldThrowException()  {
+        try {
+            assertAdd(3, "//;\n1;-2");
+            fail("did not throw Exception");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private void assertAdd(int result, String nums) throws Exception {
+            assertEquals(result, calculator.add(nums));
+
     }
 }
